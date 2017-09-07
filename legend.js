4805f44135b78e04d791ed7bb79ae0a461ccd6c0
Jurjen Knarren <408213+JurjenKnarren@users.noreply.github.com>
2017-09-07 10:04:58 +0200

    quick fix for more legend items (14)

--- legend.js
+++ legend.js
@@ -13,202 +13,7 @@ define(
 
                     appearance: {
                         uses: "settings",
-                        items: {
-                            myTextBox1: {
-                                type: "items",
-                                label: "First Legend",
-                                items: {
-                                    visibleBox: {
-                                        label: "Is Visible?",
-                                        ref: "myVisibleBox1",
-                                        type: "boolean",
-                                        component: "switch",
-                                        defaultValue: true,
-                                        options: [{
-                                                value: true,
-                                            },
-                                            {
-                                                value: false
-
-                                            }
-                                        ]
-                                    },
-                                    textBox: {
-                                        label: "Name",
-                                        ref: "myTextBox1",
-                                        type: "string",
-                                        expression: "optional"
-                                    },
-                                    colorBox: {
-                                        label: "Color",
-                                        ref: "myColorBox1",
-                                        type: "string",
-                                        expression: "optional"
-                                    },
-                                    typeBox: {
-                                        label: "Line/Box",
-                                        ref: "myTypeBox1",
-                                        type: "boolean",
-                                        component: "switch",
-                                        defaultValue: false,
-                                        options: [{
-                                                value: true,
-                                            },
-                                            {
-                                                value: false
-
-                                            }
-                                        ]
-                                    }
-                                }
-                            },
-                            myTextBox2: {
-                                type: "items",
-                                label: "Second Legend",
-                                items: {
-
-                                    visibleBox: {
-                                        label: "Is Visible?",
-                                        ref: "myVisibleBox2",
-                                        type: "boolean",
-                                        component: "switch",
-                                        defaultValue: true,
-                                        options: [{
-                                                value: true,
-                                            },
-                                            {
-                                                value: false
-
-                                            }
-                                        ]
-                                    },
-                                    textBox: {
-                                        label: "Name",
-                                        ref: "myTextBox2",
-                                        type: "string",
-                                        expression: "optional"
-                                    },
-                                    colorBox: {
-                                        label: "Color",
-                                        ref: "myColorBox2",
-                                        type: "string",
-                                        expression: "optional"
-                                    },
-                                    typeBox: {
-                                        label: "Line/Box",
-                                        ref: "myTypeBox2",
-                                        type: "boolean",
-                                        component: "switch",
-                                        defaultValue: false,
-                                        options: [{
-                                                value: true,
-                                            },
-                                            {
-                                                value: false
-
-                                            }
-                                        ]
-                                    }
-                                }
-                            },
-                            myTextBox3: {
-                                type: "items",
-                                label: "Third Legend",
-                                items: {
-                                    visibleBox: {
-                                        label: "Is Visible?",
-                                        ref: "myVisibleBox3",
-                                        type: "boolean",
-                                        component: "switch",
-                                        defaultValue: false,
-                                        options: [{
-                                                value: true,
-                                            },
-                                            {
-                                                value: false
-
-                                            }
-                                        ]
-                                    },
-                                    textBox: {
-                                        label: "Name",
-                                        ref: "myTextBox3",
-                                        type: "string",
-                                        expression: "optional"
-                                    },
-                                    colorBox: {
-                                        label: "Color",
-                                        ref: "myColorBox3",
-                                        type: "string",
-                                        expression: "optional"
-                                    },
-                                    typeBox: {
-                                        label: "Line/Box",
-                                        ref: "myTypeBox3",
-                                        type: "boolean",
-                                        component: "switch",
-                                        defaultValue: false,
-                                        options: [{
-                                                value: true,
-                                            },
-                                            {
-                                                value: false
-
-                                            }
-                                        ]
-                                    }
-                                }
-                            },
-                            myTextBox4: {
-                                type: "items",
-                                label: "Fourth Legend",
-                                items: {
-                                    visibleBox: {
-                                        label: "Is Visible?",
-                                        ref: "myVisibleBox4",
-                                        type: "boolean",
-                                        component: "switch",
-                                        defaultValue: false,
-                                        options: [{
-                                                value: true,
-                                            },
-                                            {
-                                                value: false
-
-                                            }
-                                        ]
-                                    },
-                                    textBox: {
-                                        label: "Name",
-                                        ref: "myTextBox4",
-                                        type: "string",
-                                        expression: "optional"
-                                    },
-                                    colorBox: {
-                                        label: "Color",
-                                        ref: "myColorBox4",
-                                        type: "string",
-                                        expression: "optional"
-                                    },
-                                    typeBox: {
-                                        label: "Line/Box",
-                                        ref: "myTypeBox4",
-                                        type: "boolean",
-                                        component: "switch",
-                                        defaultValue: false,
-                                        options: [{
-                                                value: true,
-                                            },
-                                            {
-                                                value: false
-
-                                            }
-                                        ]
-                                    }
-                                }
-                            }
-
-                        }
+                        items: legendExt.getLegendItems(14) //TODO: add setting to make number of legend items dynamic
                     }
                 }
             },
@@ -219,32 +24,77 @@ define(
             },
 
             paint: function($element, layout) {
-
-
                 $element.empty();
-                var legendSpecs = {
-                    measureName: [layout.myTextBox1, layout.myTextBox2, layout.myTextBox3, layout.myTextBox4],
-                    background_color: [layout.myColorBox1, layout.myColorBox2, layout.myColorBox3, layout.myColorBox4],
-                    type: [layout.myTypeBox1, layout.myTypeBox2, layout.myTypeBox3, layout.myTypeBox4],
-                    visible: [layout.myVisibleBox1, layout.myVisibleBox2, layout.myVisibleBox3, layout.myVisibleBox4]
-
+                let nLegendItems = Object.keys(layout).filter(function (key) { return key.startsWith('myTextBox'); }).length;
+                let legendSpecs = [];
+                for (let i = 1; i <= nLegendItems; i++) {
+                    legendSpecs.push({
+                        measureName: layout['myTextBox' + i],
+                        background_color: layout['myColorBox' + i],
+                        typeIsBox: layout['myTypeBox' + i],
+                        isVisible: layout['myVisibleBox' + i]
+                    });
                 }
+
                 //console.log(document.getElementById(layout.qInfo.qId), layout.qHyperCube ) ;
-                var legend = '\
+                let legend = '\
 					<div class="legend" id="' + layout.qInfo.qId + '">';
-                var columnWidth = ($element.width()/4 )< 140 ? 140  : ($element.width()/4) ;
-                for (var i = 0; i < legendSpecs.measureName.length; i++) {
-                    if (legendSpecs.visible[i]) {
-                        let legendType = legendSpecs.type[i] ? 'box' : 'line';
-                        legend += '<div class="column" style="font-size:12px' + ';width:' + columnWidth + 'px' + '"><div class="' + legendType + '" style="background-color:' + legendSpecs.background_color[i] + '"></div>' + '<b>' + legendSpecs.measureName[i] + '</b>' + '</div>';
+                let columnWidth = ($element.width() / nLegendItems) < 140 ? 140 : ($element.width() / nLegendItems) ;
+                for (var i = 0; i < nLegendItems; i++) {
+                    if (legendSpecs[i].isVisible) {
+                        let legendType = legendSpecs[i].typeIsBox ? 'box' : 'line';
+                        legend += '<div class="column" style="font-size:12px' + ';width:' + columnWidth + 'px' + '"><div class="' + legendType + '" style="background-color:' + legendSpecs[i].background_color + '"></div>' + '<b>' + legendSpecs[i].measureName + '</b>' + '</div>';
                     }
                 }
 
                 legend += '</div>';
 
                 $element.html(legend);
-
             }
         };
     }
 );
+
+var legendExt = { 
+    getLegendItems: function (nItems) {
+        var items = [];
+        for (let i = 1; i <= nItems; i++) {
+            items["myTextBox" + i] = {
+                type: "items",
+                label: "Legend - " + i,
+                items: {
+                    visibleBox: {
+                        label: "Is Visible?",
+                        ref: "myVisibleBox" + i,
+                        type: "boolean",
+                        component: "switch",
+                        defaultValue: true,
+                        options: [{ value: true }, { value: false }]
+                    },
+                    textBox: {
+                        label: "Name",
+                        ref: "myTextBox" + i,
+                        type: "string",
+                        expression: "optional"
+                    },
+                    colorBox: {
+                        label: "Color",
+                        ref: "myColorBox" + i,
+                        type: "string",
+                        expression: "optional"
+                    },
+                    typeBox: {
+                        label: "Line/Box",
+                        ref: "myTypeBox" + i,
+                        type: "boolean",
+                        component: "switch",
+                        defaultValue: false,
+                        options: [{ value: true }, { value: false }]
+                    }
+                }
+            }
+        }
+
+        return items;
+    }
+}
